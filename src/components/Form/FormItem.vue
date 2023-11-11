1d00247ffa063212e9f77f6852851e04f5eb60b3
Alouger <850022303@qq.com>
2023-11-11 16:40:08 +0800

    Form组件，完成FormItem验证，待完成自动触发验证

--- src/components/Form/FormItem.vue
+++ src/components/Form/FormItem.vue
@@ -1,6 +1,11 @@
 <template>
   <div
     class="wow-form-item"
+    :class="{
+      'is-error': validateStatus.state === 'error',
+      'is-success': validateStatus.state === 'success',
+      'is-loading': validateStatus.loading
+    }"
   >
     <label class="wow-form-item__label">
       <!-- 具名插槽, 通过这个插槽我们可以实现对label的自定义 -->
@@ -11,6 +16,9 @@
     </label>
     <div class="wow-form-item__content">
       <slot />
+      <div class="wow-form-item__error-msg" v-if="validateStatus.state === 'error'">
+        {{ validateStatus.errorMsg }}
+      </div>
     </div>
     {{ innerValue }} - {{ itemRules}}
     <button @click.prevent="validate">Validate</button>
@@ -18,11 +26,11 @@
 </template>
 
 <script setup lang="ts">
-import { inject, computed } from 'vue'
+import { inject, computed, reactive } from 'vue'
 import Schema from 'async-validator'
 import { isNil } from 'lodash-es'
 import { formContextKey } from './types'
-import type { FormItemProps } from './types'
+import type { FormItemProps, FormValidateFailure } from './types'
 
 defineOptions({
   name: 'WowFormItem'
@@ -31,6 +39,14 @@ defineOptions({
 const props = defineProps<FormItemProps>()
 // 拿到Form组件传过来的formContext后我们还需要在FormItem上设置一个属性，让用户去决定我们到底要使用哪个数据和规则
 const formContext = inject(formContextKey)
+const validateStatus = reactive({
+  // 显示验证状态：成功，失败，初始
+  state: 'init',
+  // 验证失败反馈的错误信息
+  errorMsg: '',
+  // validate函数里是一个异步过程，我们可能会有一些自定义请求，比如远程服务器发送请求检查用户名是否被占用等情况，所以需要一个loading状态
+  loading: false
+})
 
 // 我们从formContext取得的对应的值
 const innerValue = computed(() => {
@@ -59,19 +75,24 @@ const itemRules = computed(() => {
 
 const validate = () => {
   const modelName = props.prop
-  console.log("props.prop", props.prop);
   if (modelName) {
     const validator = new Schema({
       [modelName]: itemRules.value
     })
-    console.log("validator", validator);
-    console.log("itemRules.value", itemRules.value);
+    validateStatus.loading = true
     validator.validate({ [modelName]: innerValue.value })
       .then(() => {
-        console.log('no error')
+        validateStatus.state = 'success'
       })
-      .catch(e => {
-        console.log(e.errors)
+      .catch((e: FormValidateFailure) => {
+        // 解构出errors
+        const { errors } = e
+        validateStatus.state = 'error'
+        // 这里的message有可能是undefined，所以有加个 || ''
+        validateStatus.errorMsg = (errors && errors.length > 0) ? errors[0].message || '' : ''
+      })
+      .finally(() => {
+        validateStatus.loading = false
       })
   }
 }
--- src/components/Form/types.ts
+++ src/components/Form/types.ts
@@ -1,4 +1,6 @@
 import type { InjectionKey } from 'vue'
+// RuleItem是async-validator提供的规则类型
+import type { RuleItem, ValidateError, ValidateFieldsError } from 'async-validator'
 
 export interface FormItemProps {
   label: string;
@@ -6,13 +8,20 @@ export interface FormItemProps {
   prop?: string;
 }
 
+export type FormRules = Record<string, RuleItem[]>
+
 export interface FormProps {
-  model: Record<string, any>
-  rules: Record<string, any>
+  model: Record<string, any>;
+  rules: FormRules;
 }
 
 export interface FormContext extends FormProps {
   
+}
+
+export interface FormValidateFailure {
+  errors: ValidateError[] | null;
+  fields: ValidateFieldsError;
 }
 
 export const formContextKey: InjectionKey<FormContext> = Symbol('formContextKey')
